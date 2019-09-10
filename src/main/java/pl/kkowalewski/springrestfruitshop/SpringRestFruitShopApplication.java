2a7adf1917d5966decc58a19336bc073b59d497c
Kamil Kowalewski <szamil128@onet.pl>
2019-09-10 16:53:40 +0200

    Initial commit

---
+++ src/main/java/pl/kkowalewski/springrestfruitshop/SpringRestFruitShopApplication.java
@@ -0,0 +1,12 @@
+package pl.kkowalewski.springrestfruitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestFruitShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestFruitShopApplication.class, args);
+    }
+}
