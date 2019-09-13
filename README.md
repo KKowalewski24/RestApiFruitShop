4b500efad5f792c0bfedf146cc504e504ad024c5
Kamil Kowalewski <szamil128@onet.pl>
2019-09-13 21:31:11 +0200

    Add metadate to swagger

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # RestApiFruitShop
 
+Spring Boot (ver. 2.1.8) project of Rest API Fruit Shop.
+
+Project has been initialized with Spring Initializr https://start.spring.io/
+
+### Technologies and Maven dependencies used in project
+* Maven - Project Management
+* Intellij IDEA Ultimate
+* JDK 8
+* JPA
+* H2 database
+* Mapstruct
+* Swagger
+* JUnit 4
+* Mockito
+* JaCoCo
+* Checkstyle
+* Commons-lang3
--- src/main/java/pl/kkowalewski/springrestfruitshop/config/SwaggerConfiguration.java
+++ src/main/java/pl/kkowalewski/springrestfruitshop/config/SwaggerConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 import static pl.kkowalewski.springrestfruitshop.constant.AppConstants.SLASH;
 
@@ -17,6 +21,20 @@ public class SwaggerConfiguration {
     /*------------------------ FIELDS REGION ------------------------*/
 
     /*------------------------ METHODS REGION ------------------------*/
+    private ApiInfo prepareMetaData() {
+        return new ApiInfo(
+                "Kamil Kowalewski",
+                "Spring Rest Api Fruit Shop",
+                "1.0",
+                "Feel Free",
+                new Contact("Kamil Kowalewski",
+                        "KamilKowalewski.github.io",
+                        "kamilkowalewski@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -24,6 +42,7 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping(SLASH);
+                .pathMapping(SLASH)
+                .apiInfo(prepareMetaData());
     }
 }
